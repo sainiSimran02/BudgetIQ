06c6515d760c897467668dc6e77acc1501c1c2e6
Simran <simransaini5869@gmail.com>
2026-05-24 00:05:25 +0530

    Made minor changes in frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = ({ user: propUser, onLogout }) => {
           <div className={navbarStyles.logoImage}>
             <img src={img1} alt="logo" />
           </div>
-          <span className={navbarStyles.logoText}>Expense Tracker</span>
+          <span className={navbarStyles.logoText}>BudgetIQ</span>
         </div>
 
         {/* if the user is present */}
